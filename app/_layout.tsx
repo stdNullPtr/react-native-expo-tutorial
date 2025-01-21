c8c0f1b5f56ee81d0f025abf929e2f25de4b008c
stdnullptr <antonio.liubchev@gmail.com>
2025-01-22 00:51:07 +0200

    feat(tutorial): completed https://docs.expo.dev/tutorial/gestures/

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import {Stack} from "expo-router";
 import {DarkTheme, ThemeProvider} from "@react-navigation/native";
 import {StatusBar} from "expo-status-bar";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 export default function RootLayout() {
     return (
-        <ThemeProvider value={DarkTheme}>
-            <StatusBar style="light"/>
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-                <Stack.Screen name="+not-found" />
-            </Stack>
-        </ThemeProvider>
+        <GestureHandlerRootView style={{flex: 1}}>
+            <ThemeProvider value={DarkTheme}>
+                <StatusBar style="light"/>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                    <Stack.Screen name="+not-found"/>
+                </Stack>
+            </ThemeProvider>
+        </GestureHandlerRootView>
     );
 }
